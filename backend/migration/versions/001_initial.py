178ac60ae67906aacbf00bc31d55846cc0fe9eed
guyezhimeng <164464292+guyezhimeng@users.noreply.github.com>
2026-04-22 00:06:24 +0800

    Delete backend directory

--- backend/migration/versions/001_initial.py
+++
@@ -1,4 +0,0 @@
-from app.models.task import Task, TaskTransition, TaskState
-from app.models.chat import ChatSession, ChatMessage
-from app.models.attachment import Attachment
-from app.models.outbox import OutboxEvent
